17279d817968086698dcef339b427896e03e92e6
ronaldtellwz <ronaldtellwz@gmail.com>
2025-01-10 02:23:05 +0000

    slin

--- src/components/Features.tsx
+++ src/components/Features.tsx
@@ -1,6 +1,5 @@
 'use client'
 
-import { useEffect, useState } from 'react'
 import { motion } from 'framer-motion'
 import { useInView } from 'react-intersection-observer'
 import { FaBolt, FaCog, FaUsers, FaChartLine } from 'react-icons/fa';
