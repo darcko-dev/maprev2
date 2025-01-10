15109a8f067075ee611a780f8146909d1605cbba
ronaldtellwz <ronaldtellwz@gmail.com>
2025-01-10 02:11:46 +0000

    deleted

--- src/app/admin/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function AdminPage() {
-  return (
-    <div>
-      <h1 className="text-2xl font-bold mb-4">Panel de Administración</h1>
-      <p>Selecciona una sección para editar en el menú de la izquierda.</p>
-    </div>
-  )
-}
-
